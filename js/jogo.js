4106b2476571854d797a60a9e7f6a07521d575ea
Mário André Martins <mario@MacBook-Air-de-Mario.local>
2022-02-04 09:45:16 -0300

    adicionada uma lista de palavras. adicionada a função que desenha a forca. alguns estilos para a apresentação da página

---
+++ js/jogo.js
@@ -0,0 +1,139 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let palavras = ["AMARELO",
+        "AMIGA",
+        "AMOR",
+        "AVE",
+        "AVIAO",
+        "BALAO",
+        "BEBE",
+        "BOLO",
+        "BRANCO",
+        "CAMA",
+        "CANECA",
+        "CELULAR",
+        "CLUBE",
+        "COPO",
+        "DOCE",
+        "ELEFANTE",
+        "ESCOLA",
+        "ESTOJO",
+        "FACA",
+        "FOTO",
+        "GARFO",
+        "GELEIA",
+        "GIRAFA",
+        "JANELA",
+        "LIMONADA",
+        "MAE",
+        "MEIA",
+        "NOITE",
+        "OCULOS",
+        "ONIBUS",
+        "OVO",
+        "PAI",
+        "PAO",
+        "PARQUE",
+        "PASSARINHO",
+        "PEIXE",
+        "PIJAMA",
+        "RATO",
+        "UMBIGO"];
+
+    let canvas = document.getElementById('hangman');
+    let context = canvas.getContext('2d');
+
+    const Desenhar = (part) => {
+        switch (part) {
+            case 'forca':
+                context.strokeStyle = '#0';
+                context.lineWidth = 2;
+                context.beginPath();
+                context.moveTo(900, 900);
+                context.lineTo(100, 900);
+                context.moveTo(300, 900);
+                context.lineTo(300, 100);
+                context.lineTo(600, 100);
+                context.lineTo(600, 200);
+                context.stroke();
+                break;
+
+            case 'cabeça':
+                context.lineWidth = 2;
+                context.beginPath();
+                context.arc(600, 260, 60, 0, Math.PI * 2);
+                context.closePath();
+                context.stroke();
+                break;
+
+            case 'corpo':
+                context.lineWidth = 2;
+                context.beginPath();
+                context.moveTo(600, 320);
+                context.lineTo(600, 600);
+                context.stroke();
+                break;
+
+            case 'braços':
+                context.lineWidth = 2;
+                context.beginPath();
+                context.moveTo(600, 400);
+                context.lineTo(500, 500);
+                context.moveTo(600, 400);
+                context.lineTo(700, 500);
+                context.stroke();
+                break;
+
+            case 'pernas':
+                context.lineWidth = 2;
+                context.beginPath();
+                context.moveTo(600, 600);
+                context.lineTo(550, 800);
+                context.moveTo(600, 600);
+                context.lineTo(650, 800);
+                context.stroke();
+                break;
+
+            case 'olhos':
+                context.lineWidth = 2;
+                context.beginPath();
+                context.arc(580, 250, 10, 0, Math.PI * 2);
+                context.closePath(620, 250);
+                context.stroke();
+                context.beginPath();
+                context.arc(620, 250, 10, 0, Math.PI * 2);
+                context.closePath();
+                context.stroke();
+                break;
+
+            case 'nariz':
+                context.lineWidth = 2;
+                context.beginPath();
+                context.moveTo(600, 260);
+                context.lineTo(605, 275);
+                context.lineTo(600, 275);
+                context.stroke();
+                break;
+
+            case 'boca':
+                context.lineWidth = 2;
+                context.beginPath();
+                context.moveTo(615, 295);
+                context.lineTo(585, 295);
+                context.stroke();
+                break;
+
+        }
+
+    };
+
+    Desenhar('forca');
+    Desenhar('cabeça');
+    Desenhar('corpo');
+    Desenhar('braços');
+    Desenhar('pernas');
+    Desenhar('olhos');
+    Desenhar('nariz');
+    Desenhar('boca');
+
+})
---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var listaDePalavras = [];
+
+    let btnEnviar = document.getElementById('btnEnviar');
+    let lista = document.getElementById('lista_de_palavras');
+
+    btnEnviar.addEventListener('click', () => {
+        let adicionarPalavra = document.getElementById('adicionarPalavra');
+
+        listaDePalavras.push(adicionarPalavra.value);
+
+        let item = document.createElement('li');
+        item.textContent = adicionarPalavra.value;
+        lista.appendChild(item);
+
+        adicionarPalavra.value = "";
+    })
+
+})
